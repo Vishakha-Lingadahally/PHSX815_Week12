cfefe8562f68adf80eaee0f26d5f5534edc0e945
Vishakha-Lingadahally <78513161+Vishakha-Lingadahally@users.noreply.github.com>
2021-04-27 00:49:06 +0200

    Update university_admissions.py

--- university_admissions.py
+++ university_admissions.py
@@ -7,19 +7,17 @@ Created on Mon Apr 26 15:33:12 2021
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-col_list=["Degree of urbanization (Urban-centric locale)"]
+col_list=["Percent of freshmen receiving federal, state, local or institutional grant aid"]
 col_list1=["Percent of total enrollment that are women"]
 df=pd.read_csv("IPEDS_data.csv", usecols=col_list)
 dg=pd.read_csv("IPEDS_data.csv", usecols=col_list1)
-print(df["Degree of urbanization (Urban-centric locale)"])
+print(df["Percent of freshmen receiving federal, state, local or institutional grant aid"])
 print(dg["Percent of total enrollment that are women"])
-dfedges, dgedges = np.linspace(-4, 4, 42), np.linspace(-25, 25, 42)
-hist, dfedges, dgedges = np.histogram2d(df, dg, (dfedges, dgedges))
-xidx = np.clip(np.digitize(df, dfedges), 0, hist.shape[0]-1)
-yidx = np.clip(np.digitize(dg, dgedges), 0, hist.shape[1]-1)
-c = hist[xidx, yidx]
-plt.scatter(df, dg, c=c)
 
-plt.show()
+for col in df.columns:
+    plt.scatter(df["Percent of freshmen receiving federal, state, local or institutional grant aid"], dg["Percent of total enrollment that are women"], label=col)
+    plt.legend(loc='best', fontsize=16)
+    plt.xlabel("Percent of freshmen receiving federal, state, local or institutional grant aid")
+    plt.ylabel('Percent of total enrollment that are women')
+    plt.show()
